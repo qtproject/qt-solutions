42e9ef94b88168fb58c895ad065690c03b6d672a
Yunky <yunkysoft@yeah.net>
2021-09-15 09:22:22 +0800

    Update qtsingleapplication.cpp
    Fix the problem that the window cannot be activated when the program is minimized to the system tray.

--- qtsingleapplication/src/qtsingleapplication.cpp
+++ qtsingleapplication/src/qtsingleapplication.cpp
@@ -323,9 +323,9 @@ QWidget* QtSingleApplication::activationWindow() const
 void QtSingleApplication::activateWindow()
 {
     if (actWin) {
-        actWin->setWindowState(actWin->windowState() & ~Qt::WindowMinimized);
-        actWin->raise();
+        actWin->setWindowFlags(Qt::Window);
         actWin->activateWindow();
+        actWin->show();
     }
 }
 
